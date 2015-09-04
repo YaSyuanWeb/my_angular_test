616d94613b12093a099f5a6d5f4fc02cb9c0024d
Jumei <js2022@cumc.columbia.edu>
2015-09-04 16:48:15 -0400

    test angular

---
+++ js/controllers.js
@@ -0,0 +1,9 @@
+var artistControllers = angular.module('artistControllers',[]);
+
+artistControllers.controller('ListController', ['$scope','$http', function MyController($scope, $http){
+
+	$http.get('js/data.json').success(function(data) {
+		$scope.artists = data;
+		$scope.artistOrder = 'name';
+	});
+}]);
